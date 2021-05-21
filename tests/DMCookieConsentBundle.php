7388dd2f45d48abcbedf5c37c9f64255d99c3276
bruno <dimicelibruno@gmail.com>
2021-05-22 00:37:47 +0200

    Add PHPUnit

---
+++ tests/DMCookieConsentBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DiMiceli\CookieConsentBundle\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DMCookieConsentBundle extends TestCase
+{
+    public function testFuck()
+    {
+        $this->assertTrue(true);
+    }
+}
