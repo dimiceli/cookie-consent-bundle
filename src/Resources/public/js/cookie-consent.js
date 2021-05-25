383c176a3ee566c94de7963abfc2da96eb8407a6
bruno <dimicelibruno@gmail.com>
2021-05-25 12:49:10 +0200

    copyright

--- src/Resources/public/js/cookie-consent.js
+++ src/Resources/public/js/cookie-consent.js
@@ -1,3 +1,12 @@
+/*
+ *  This file is part of the Symfony package.
+ *  
+ *  (c) Bruno Di Miceli <dimicelibruno@gmail.com>
+ *  
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
 document.addEventListener("DOMContentLoaded", function() {
     var cookieConsentForm = document.forms['cookie_consent'];
     var cookieConsentFormButtonAccept = document.getElementById('dm-cookie-consent-button-accept');
